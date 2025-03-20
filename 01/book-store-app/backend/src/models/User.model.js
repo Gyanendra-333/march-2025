2dc75ddc6257350ccdc83afa27dd583a0c8c0903
Gyanendra <pgyanendra333@gmail.com>
2025-03-20 18:41:45 +0530

    create auth and books controler and complete backend portion

--- 01/book-store-app/backend/src/models/User.model.js
+++ 01/book-store-app/backend/src/models/User.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema
     ({
@@ -22,6 +23,21 @@ const userSchema = new mongoose.Schema
             default: ""
         }
     }, { timestamps: true });
+
+
+// hash password before saving user to db 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+// match or compare password 
+userSchema.methods.comparePassword = async function (userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
--- 01/book-store-app/backend/src/routes/authRoutes.js
+++ 01/book-store-app/backend/src/routes/authRoutes.js
@@ -1,7 +1,12 @@
-import express, { Router } from "express";
+import { Router } from "express";
 import User from "../models/User.model.js";
+import jwt from "jsonwebtoken";
 
 const router = Router();
+
+const generateToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+}
 
 
 // Register 
@@ -31,15 +36,61 @@ router.post("/register", async (req, res) => {
             profileImage: profileImage
         })
 
+        await user.save();
+
+        const token = generateToken(user._id);
+
+        return res.status(201).json({
+            token, user: {
+                id: user._id,
+                userName: user.userName,
+                email: user.email,
+                profileImage: user.profileImage
+            }
+        })
+
     } catch (error) {
-        console.log(error);
+        console.log("Error in register", error);
+        return res.status(500).json({ message: "Internal server error" })
     }
 })
 
 
 // Login 
 router.post("/login", async (req, res) => {
-    res.send("login")
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields is required" })
+        }
+
+        // check if user is existed 
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "user does not exist" })
+        }
+
+        const isPasswordCorrect = await user.comparePassword(password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "invalid credentials" })
+        }
+
+        const token = generateToken(user._id);
+
+        return res.status(201).json({
+            token, user: {
+                id: user._id,
+                userName: user.userName,
+                email: user.email,
+                profileImage: user.profileImage,
+            },
+        });
+
+
+    } catch (error) {
+        console.log("Error in Login", error);
+        return res.status(500).json({ message: "Internal server error" })
+    }
 })
 
 export default router;
