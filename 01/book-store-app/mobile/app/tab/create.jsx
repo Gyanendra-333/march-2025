7372a809f5aa8e9f9c5f6120d755bc7d95226772
Gyanendra <pgyanendra333@gmail.com>
2025-03-24 18:43:25 +0530

    create page

--- 01/book-store-app/mobile/app/tab/create.jsx
+++ 01/book-store-app/mobile/app/tab/create.jsx
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { useRouter } from "expo-router";
+import styles from "../../assets/styles/create.styles"
 
 export default function create() {
+    const [title, setTitle] = useState("");
+    const [caption, setCaption] = useState("");
+    const [rating, setRating] = useState(3);
+    const [image, setImage] = useState(null);
+    const [imageBase64, setImageBase64] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const router = useRouter();
+
+    const pickImage = async () => { };
+
+    const handleSubmit = async () => { };
+
     return (
-        <View>
-            <Text>create</Text>
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <ScrollView contentContainerStyle={styles.container} style={styles.scrollViewStyle}>
+                <View style={styles.card}>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>Add Book Recommendation</Text>
+                        <Text style={styles.subtitle}>Share your favourite books with others</Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
