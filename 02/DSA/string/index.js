8299f0110d8207396435c8534ff3f03a727e2e62
Gyanendra <pgyanendra333@gmail.com>
2025-04-15 18:56:52 +0530

    dsa string

--- 02/DSA/string/index.js
+++ 02/DSA/string/index.js
@@ -1,3 +1,25 @@
 
 
+const arr = [10, 20, 30, 40, 50]
+let temp = 0;
+for (let i = 0; i < arr.length; i++) {
+    temp += arr[i]
+}
+// console.log(temp);
+
+
 let str = "gyanendra"
+let strTotal = ""
+for (let i = 0; i < str.length; i++) {
+    strTotal += str[i]
+}
+console.log(strTotal);
+
+
+
+let revStr = "gyanendra"
+let revStrTotal = "Reverse Name : "
+for (let i = revStr.length - 1; i >= 0; i--) {
+    revStrTotal += revStr[i]
+}
+console.log(revStrTotal);
